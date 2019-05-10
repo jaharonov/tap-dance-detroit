7c76d77abe16bd329f2f5d246708e3e326ef498a
Jaffa Aharonov <j.aharonov@gmail.com>
2019-05-10 11:12:33 -0500

    changed order faculty

--- src/components/Faculty.vue
+++ src/components/Faculty.vue
@@ -5,6 +5,23 @@
     </mdb-row>
     <mdb-row class="mx-auto pt-5">
       <mdb-col lg="4">
+          <h2 class="red-text mx-auto text-center big pt-3">DENISE CASTON</h2>
+          <h4 class="grey-text mx-auto text-center big">FOUNDER/DIRECTOR</h4>
+          <img src="/static/faculty_denise-caston.jpg" class="img-fluid-grow mx-auto d-block" style="max-width: 100%"/>
+      </mdb-col>
+      <mdb-col lg="8" class="text-justify pt-3">
+          <p class="pt-3">Denise Caston was a Radio City Rockette for 10 years and toured nationally with the Tony Award winning musical, Crazy For You. Denise has also had the honor of performing the choreography of Savion Glover at a showcase in New York City. She performed on tour in Australia, dancing all over the continent in Elvis to the Max. Her choreography has been seen in many productions in which she performed, including shows at Caesars Windsor in Ontario, Canada. Credits also include film, television, commercials, and industrials.</p>
+
+<p>She currently teaches at Broadway Dance Center and Steps on Broadway. Other teaching credits include Beyond The Stars Dance Convention, Phoenix Tap Fest, OKC Tap Fest, Gregg Russell's Tap Into the Network, Tradition In Tap, and The Rockette Experience© at Radio City Music Hall.</p>
+
+<p>In 2011, Denise was invited to speak at the TEDx Detroit Conference about her contributions to the cultural landscape in Detroit, including founding and producing Motor City Tap Fest and founding the Detroit Tap Repertory.</p>
+
+<p>In 2014, she was awarded a prestigious MaTilDa Award from her alma mater, Oakland University, for Alumni Achievement in Dance.</p>
+
+</mdb-col>
+    </mdb-row>
+    <mdb-row class="mx-auto pt-5">
+      <mdb-col lg="4">
           <h2 class="red-text mx-auto text-center big">KELSEY ROSE YOUNG</h2><img src="/static/faculty_kelsey-rose-young.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
       </mdb-col>
       <mdb-col lg="8" class="text-justify pt-3">
@@ -19,21 +36,6 @@
 <p>As a choreographer, Kelsey draws from her training and range of experiences as a singer and dancer to create a style that is intricate, gritty, passionate, and entertaining. Her pieces have received many awards and accolades across the country and she is quickly becoming one of the Midwest’s premier tap choreographers.</p>
 
 <p>Kelsey is a member of Cecchetti Council of America and has also studied and competed in ballroom dancing. She works independently as a choreographer for students throughout the Nation. Over the years, Kelsey has worked with Jay Sean, the Jonas Brothers, Debbie Gibson, and Radio Disney. She has performed in the Detroit, Macy’s and Chicago Thanksgiving Day Parades, at the Detroit Jazz Festival, and at TedX Detroit. She most recently has performed and choreographed a half time show for the NBA’s Detroit Pistons.</p>
-</mdb-col>
-    </mdb-row>
-    <mdb-row class="mx-auto pt-5">
-      <mdb-col lg="4">
-          <h2 class="red-text mx-auto text-center big">DENISE CASTON</h2><img src="/static/faculty_denise-caston.jpg" class="img-fluid-grow mx-auto d-block" style="max-width: 100%"/>
-      </mdb-col>
-      <mdb-col lg="8" class="text-justify pt-3">
-          <p class="pt-3">Denise Caston was a Radio City Rockette for 10 years and toured nationally with the Tony Award winning musical, Crazy For You. Denise has also had the honor of performing the choreography of Savion Glover at a showcase in New York City. She performed on tour in Australia, dancing all over the continent in Elvis to the Max. Her choreography has been seen in many productions in which she performed, including shows at Caesars Windsor in Ontario, Canada. Credits also include film, television, commercials, and industrials.</p>
-
-<p>She currently teaches at Broadway Dance Center and Steps on Broadway. Other teaching credits include Beyond The Stars Dance Convention, Phoenix Tap Fest, OKC Tap Fest, Gregg Russell's Tap Into the Network, Tradition In Tap, and The Rockette Experience© at Radio City Music Hall.</p>
-
-<p>In 2011, Denise was invited to speak at the TEDx Detroit Conference about her contributions to the cultural landscape in Detroit, including founding and producing Motor City Tap Fest and founding the Detroit Tap Repertory.</p>
-
-<p>In 2014, she was awarded a prestigious MaTilDa Award from her alma mater, Oakland University, for Alumni Achievement in Dance.</p>
-
 </mdb-col>
     </mdb-row>
   </mdb-container>
