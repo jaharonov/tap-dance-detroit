50b788ce2f72a81efc77b1ea19a62305581c4c2d
Jaffa Aharonov <j.aharonov@gmail.com>
2019-07-29 10:34:20 -0500

    italics beatles

--- src/components/TddPresents.vue
+++ src/components/TddPresents.vue
@@ -7,7 +7,7 @@
         <h2 class="mx-auto text-center p-3 red-text big" style="text-transform: uppercase;">November 9th-10th</h2>
     </mdb-row>
     <mdb-row class="mx-auto pt-2">
-        <h2 class="mx-auto text-center p-3 red-text big" style="text-transform: uppercase;">2 levels, 2 days of classes, followed by Beatles On Tap at the Magic Bag Theatre! </h2>
+        <h2 class="mx-auto text-center p-3 red-text big" style="text-transform: uppercase;">2 levels, 2 days of classes, followed by <i>Beatles On Tap</i> at the Magic Bag Theatre! </h2>
     </mdb-row>
     <mdb-row class="mx-auto pt-2">
         <h4 class="mx-auto text-center p-3 grey-text big" style="text-transform: uppercase;">schedule and registration coming soon!</h4>
