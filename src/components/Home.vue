c32f83cac8108f3a225ca17e3728c1f123d01c09
Jaffa Aharonov <j.aharonov@gmail.com>
2019-08-27 11:10:07 -0500

    removed lingering buy button

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -17,7 +17,7 @@
       <mdb-col lg="2">
           
       </mdb-col>
-      <mdb-col lg="4" class="pt-5">
+      <mdb-col lg="4" class="pt-1">
           <img src="/static/beatles-on-tap-show.png" class="img-fluid-grow mx-auto d-block" style="max-width: 100%"/>
           <div class="text-center"><a href="https://www.ticketweb.com/event/beatles-on-tap-the-magic-bag-tickets/9815475?pl=magicbag" target="_blank"><mdb-btn class="btn-grad">Tickets</mdb-btn></a></div>
       </mdb-col>
@@ -127,21 +127,13 @@
 </template>
 
 <script>
-  // import { mdbParallax, mdbContainer, mdbMask, mdbRow, mdbCol } from 'mdbvue';
-  // export default {
-  //   components: {
-  //     mdbParallax,
-  //     mdbContainer,
-  //     mdbMask,
-  //     mdbRow,
-  //     mdbCol
-  //   }
-  // }
-  
-  import { mdbContainer, mdbRow, mdbCol, mdbNavbar, mdbNavbarToggler, mdbNavbarNav, mdbNavItem, mdbInput, mdbView, mdbMask, mdbBtn, mdbNavbarBrand, mdbParallax,mdbModal, mdbModalHeader, mdbModalTitle, mdbModalBody, mdbModalFooter } from 'mdbvue';
+import { mdbTbl, mdbTblHead, mdbTblBody, mdbContainer, mdbRow, mdbCol, mdbNavbar, mdbNavbarToggler, mdbNavbarNav, mdbNavItem, mdbIcon, mdbView, mdbMask, mdbBtn, mdbNavbarBrand, mdbParallax,mdbModal, mdbModalHeader, mdbModalTitle, mdbModalBody, mdbModalFooter } from 'mdbvue';
   export default {
     name: 'AppPage',
     components: {
+      mdbTbl,
+      mdbTblHead,
+      mdbTblBody,
       mdbContainer,
       mdbRow,
       mdbCol,
@@ -149,7 +141,7 @@
       mdbNavbarToggler,
       mdbNavbarNav,
       mdbNavItem,
-      mdbInput,
+      mdbIcon,
       mdbView,
       mdbMask,
       mdbBtn,
@@ -165,248 +157,247 @@
       return {
         showModal: false
       };
-    },
-    created () {
-    (function () {
-      // var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
-      if (window.ShopifyBuy) {
-        if (window.ShopifyBuy.UI) {
-          ShopifyBuyInit();
-        } else {
-          loadScript();
-
-        }
-      } else {
-        loadScript();
-      }
-
-      function loadScript() {
-        var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
-        var script = document.createElement('script');
-        script.async = true;
-        script.src = scriptURL;
-        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
-        script.onload = ShopifyBuyInit;
-      }
-
-      function ShopifyBuyInit() {
-        var client = ShopifyBuy.buildClient({
-          domain: 'overtaps.myshopify.com',
-          storefrontAccessToken: '244fc4d27a412190862e1ff0232cb6d0',
-        });
-
-        ShopifyBuy.UI.onReady(client).then(function (ui) {
-          ui.createComponent('collection', {
-            id: [3959260741743],
-            node: document.getElementById('product-component-e7de69b923f'),
-            moneyFormat: '%24%7B%7Bamount%7D%7D',
-            options: {
-"product": {
-    "variantId": "all",
-    "width": "240px",
-    "contents": {
-      "imgWithCarousel": false,
-      "variantTitle": false,
-      "description": false,
-      "buttonWithQuantity": false,
-      "quantity": false
-    },
-    "styles": {
-      "product": {
-        "@media (max-width: 601px)": {
-          "max-width": "100%",
-          "margin-left": "10",
-          "margin-right": "10",
-          "margin-bottom": "50px",
-
-        }
-      },
-      "button": {
-        "background-color": "#ff5252",
-        "font-family": "Noto Sans KR, sans-serif",
-        ":hover": {
-          "background-color": "#ff1744"
-        },
-        ":focus": {
-          "background-color": "#ff1744"
-        },
-      },
-      "variantTitle": {
-        "font-family": "Noto Sans KR, sans-serif"
-      },
-      "title": {
-        "font-family": "Noto Sans KR, sans-serif"
-      },
-      "description": {
-        "font-family": "Noto Sans KR, sans-serif"
-      },
-      "price": {
-        "font-family": "Noto Sans KR, sans-serif"
-      },
-      "compareAt": {
-        "font-size": "12px",
-        "font-family": "Noto Sans KR, sans-serif"
-      }
-    },
-    "googleFonts": [
-      "Roboto Condensed",
-      "Roboto Condensed",
-      "Roboto Condensed",
-      "Roboto Condensed",
-      "Roboto Condensed",
-      "Roboto Condensed"
-    ]
-  },
-  "cart": {
-    "contents": {
-      "button": true,
-      "note": true
-    },
-    "text": {
-      "notice": 'Shipping and discount codes are added at checkout.',
-      "noteDescription": 'Student Name'
-    },
-    "styles": {
-      "button": {
-        "background-color": "#ff5252",
-        "font-family": "Noto Sans KR, sans-serif",
-        ":hover": {
-          "background-color": "#ff1744"
-        },
-        ":focus": {
-          "background-color": "ff1744"
-        },
-      },
-      "footer": {
-        "background-color": "#ffffff"
-      }
-    },
-    "googleFonts": [
-      "Roboto Condensed"
-    ]
-  },
-  "modalProduct": {
-    "contents": {
-      "img": false,
-      "imgWithCarousel": true,
-      "variantTitle": false,
-      "buttonWithQuantity": true,
-      "button": false,
-      "quantity": false
-    },
-    "styles": {
-      "product": {
-        "@media (min-width: 601px)": {
-          "max-width": "100%",
-          "margin-left": "0px",
-          "margin-bottom": "0px"
-        }
-      },
-      "button": {
-        "background-color": "#ff5252",
-        "font-family": "Noto Sans KR, sans-serif",
-        ":hover": {
-          "background-color": "#ff1744"
-        },
-        ":focus": {
-          "background-color": "#da611f"
-        },
-      },
-      "variantTitle": {
-        "font-family": "Noto Sans KR, sans-serif",
-      },
-      "title": {
-        "font-family": "Noto Sans KR, sans-serif"
-      },
-      "description": {
-        "font-family": "Noto Sans KR, sans-serif",
-      },
-      "price": {
-        "font-family": "Noto Sans KR, sans-serif",
-      },
-      "compareAt": {
-        "font-family": "Noto Sans KR, sans-serif",
-      }
-    },
-    "googleFonts": [
-      "Roboto Condensed",
-      "Roboto Condensed",
-      "Roboto Condensed",
-      "Roboto Condensed",
-      "Roboto Condensed",
-      "Roboto Condensed"
-    ]
-  },
-  "toggle": {
-    "styles": {
-      "toggle": {
-        "font-family": "Noto Sans KR, sans-serif",
-        "background-color": "#ff5252",
-        ":hover": {
-          "background-color": "#ff1744"
-        },
-        ":focus": {
-          "background-color": "#ff1744"
-        },
-      }
-    },
-    "googleFonts": [
-      "Roboto Condensed"
-    ]
-  },
-  "option": {
-    "styles": {
-      "label": {
-        "font-family": "Noto Sans KR, sans-serif",
-      },
-      "select": {
-        "font-family": "Noto Sans KR, sans-serif",
-      }
-    },
-    "googleFonts": [
-      "Roboto Condensed",
-      "Roboto Condensed"
-    ]
-  },
-  "productSet": {
-    "styles": {
-      "products": {
-        "@media (min-width: 601px)": {
-          "margin-left": "-20px"
-        }
-      }
     }
   }
-}
-          });
-        });
-      }
-    })();
-    this.fetchData()
-  },
-  watch: {
+</script>
+//     },
+//     created () {
+//     (function () {
+      
+//       if (window.ShopifyBuy) {
+//         if (window.ShopifyBuy.UI) {
+//           ShopifyBuyInit();
+//         } else {
+//           loadScript();
+
+//         }
+//       } else {
+//         loadScript();
+//       }
+
+//       function loadScript() {
+//         var scriptURL = 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js';
+//         var script = document.createElement('script');
+//         script.async = true;
+//         script.src = scriptURL;
+//         (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
+//         script.onload = ShopifyBuyInit;
+//       }
+
+//       function ShopifyBuyInit() {
+//         var client = ShopifyBuy.buildClient({
+//           domain: 'overtaps.myshopify.com',
+//           storefrontAccessToken: '244fc4d27a412190862e1ff0232cb6d0',
+//         });
+
+//         ShopifyBuy.UI.onReady(client).then(function (ui) {
+//           ui.createComponent('collection', {
+//             id: [3959260741743],
+//             node: document.getElementById('product-component-e7de69b923f'),
+//             moneyFormat: '%24%7B%7Bamount%7D%7D',
+//             options: {
+// "product": {
+//     "variantId": "all",
+//     "width": "240px",
+//     "contents": {
+//       "imgWithCarousel": false,
+//       "variantTitle": false,
+//       "description": false,
+//       "buttonWithQuantity": false,
+//       "quantity": false
+//     },
+//     "styles": {
+//       "product": {
+//         "@media (max-width: 601px)": {
+//           "max-width": "100%",
+//           "margin-left": "10",
+//           "margin-right": "10",
+//           "margin-bottom": "50px",
+
+//         }
+//       },
+//       "button": {
+//         "background-color": "#ff5252",
+//         "font-family": "Noto Sans KR, sans-serif",
+//         ":hover": {
+//           "background-color": "#ff1744"
+//         },
+//         ":focus": {
+//           "background-color": "#ff1744"
+//         },
+//       },
+//       "variantTitle": {
+//         "font-family": "Noto Sans KR, sans-serif"
+//       },
+//       "title": {
+//         "font-family": "Noto Sans KR, sans-serif"
+//       },
+//       "description": {
+//         "font-family": "Noto Sans KR, sans-serif"
+//       },
+//       "price": {
+//         "font-family": "Noto Sans KR, sans-serif"
+//       },
+//       "compareAt": {
+//         "font-size": "12px",
+//         "font-family": "Noto Sans KR, sans-serif"
+//       }
+//     },
+//     "googleFonts": [
+//       "Roboto Condensed",
+//       "Roboto Condensed",
+//       "Roboto Condensed",
+//       "Roboto Condensed",
+//       "Roboto Condensed",
+//       "Roboto Condensed"
+//     ]
+//   },
+//   "cart": {
+//     "contents": {
+//       "button": true,
+//       "note": true
+//     },
+//     "text": {
+//       "notice": 'Shipping and discount codes are added at checkout.',
+//       "noteDescription": 'Student Name'
+//     },
+//     "styles": {
+//       "button": {
+//         "background-color": "#ff5252",
+//         "font-family": "Noto Sans KR, sans-serif",
+//         ":hover": {
+//           "background-color": "#ff1744"
+//         },
+//         ":focus": {
+//           "background-color": "ff1744"
+//         },
+//       },
+//       "footer": {
+//         "background-color": "#ffffff"
+//       }
+//     },
+//     "googleFonts": [
+//       "Roboto Condensed"
+//     ]
+//   },
+//   "modalProduct": {
+//     "contents": {
+//       "img": false,
+//       "imgWithCarousel": true,
+//       "variantTitle": false,
+//       "buttonWithQuantity": true,
+//       "button": false,
+//       "quantity": false
+//     },
+//     "styles": {
+//       "product": {
+//         "@media (min-width: 601px)": {
+//           "max-width": "100%",
+//           "margin-left": "0px",
+//           "margin-bottom": "0px"
+//         }
+//       },
+//       "button": {
+//         "background-color": "#ff5252",
+//         "font-family": "Noto Sans KR, sans-serif",
+//         ":hover": {
+//           "background-color": "#ff1744"
+//         },
+//         ":focus": {
+//           "background-color": "#da611f"
+//         },
+//       },
+//       "variantTitle": {
+//         "font-family": "Noto Sans KR, sans-serif",
+//       },
+//       "title": {
+//         "font-family": "Noto Sans KR, sans-serif"
+//       },
+//       "description": {
+//         "font-family": "Noto Sans KR, sans-serif",
+//       },
+//       "price": {
+//         "font-family": "Noto Sans KR, sans-serif",
+//       },
+//       "compareAt": {
+//         "font-family": "Noto Sans KR, sans-serif",
+//       }
+//     },
+//     "googleFonts": [
+//       "Roboto Condensed",
+//       "Roboto Condensed",
+//       "Roboto Condensed",
+//       "Roboto Condensed",
+//       "Roboto Condensed",
+//       "Roboto Condensed"
+//     ]
+//   },
+//   "toggle": {
+//     "styles": {
+//       "toggle": {
+//         "font-family": "Noto Sans KR, sans-serif",
+//         "background-color": "#ff5252",
+//         ":hover": {
+//           "background-color": "#ff1744"
+//         },
+//         ":focus": {
+//           "background-color": "#ff1744"
+//         },
+//       }
+//     },
+//     "googleFonts": [
+//       "Roboto Condensed"
+//     ]
+//   },
+//   "option": {
+//     "styles": {
+//       "label": {
+//         "font-family": "Noto Sans KR, sans-serif",
+//       },
+//       "select": {
+//         "font-family": "Noto Sans KR, sans-serif",
+//       }
+//     },
+//     "googleFonts": [
+//       "Roboto Condensed",
+//       "Roboto Condensed"
+//     ]
+//   },
+//   "productSet": {
+//     "styles": {
+//       "products": {
+//         "@media (min-width: 601px)": {
+//           "margin-left": "-20px"
+//         }
+//       }
+//     }
+//   }
+// }
+//           });
+//         });
+//       }
+//     })();
+//     this.fetchData()
+//   },
+//   watch: {
     // call again the method if the route changes
-    '$route': 'fetchData'
-  },
-  methods: {
-    fetchData () {
-      this.error = this.post = null
-      this.loading = true
-      // replace `getPost` with your data fetching util / API wrapper
-      getPost(this.$route.params.id, (err, post) => {
-        this.loading = false
-        if (err) {
-          this.error = err.toString()
-        } else {
-          this.post = post
-        }
-      })
-    }
-  }
-}
-</script>
-
-</script>
+  //   '$route': 'fetchData'
+  // },
+  // methods: {
+  //   fetchData () {
+  //     this.error = this.post = null
+  //     this.loading = true
+  //     // replace `getPost` with your data fetching util / API wrapper
+  //     getPost(this.$route.params.id, (err, post) => {
+  //       this.loading = false
+  //       if (err) {
+  //         this.error = err.toString()
+  //       } else {
+  //         this.post = post
+  //       }
+  //     })
+  //   }
+  // }
 <style>
 
   /* .classic-form-page .gradient {
