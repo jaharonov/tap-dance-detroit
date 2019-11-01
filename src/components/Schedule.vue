cf4a779e0485566ff2255c4eca995939bb598bb4
Jaffa Aharonov <j.aharonov@gmail.com>
2019-11-01 09:09:16 -0500

    fall update

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -410,7 +410,6 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
     <div class="align-items-center justify-content-start pl-3">
     <span class="red-text"><mdb-icon far icon="calendar-alt" size="2x"/> <strong class="big">Mondays or Wednesdays 4:30pm-5:00pm or Saturdays from 12:00pm-12:30pm</strong></span>
     <p class="big">This 8-week session is for 4-5 year-olds to discover the joy of tap dancing, learning basic steps and participating in fun interactive activities that will teach them spacial awareness and musicality. The dancers will show off what they've learned after the last class.</p>
-	<p><p>Fall 1: 8-week session starting the week of September 9th and ending the week of October 28th. Saturday class will begin September 14 and go through November 2. $100</p>
 	<p>Fall 2: 6-week session starting on the week of November 4th and ending the week of December 16th. $75</p>
 	</div>
   <!-- <div class="mt-5 align-items-center justify-content-start p-3">
