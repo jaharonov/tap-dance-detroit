8969ff4682607709f5feb4707ea350fba66073dc
Jaffa Aharonov <j.aharonov@gmail.com>
2020-03-10 12:41:46 -0500

    spring updates

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -76,13 +76,12 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 				<a href="https://dancestudio-pro.com/online/index.php?account_id=15325" target="_blank"><mdb-btn outline="red">Register for classes!</mdb-btn></a>
 	</div>
 	</mdb-row>
-	</section>
 <section class="demo-section pt-5" id="schedule">
-	<h2 class="blue-text text-center"><mdb-icon icon="snowflake" size="2x" /><strong> WINTER SCHEDULE </strong><mdb-icon icon="snowflake" size="2x" /></h2>
+	<h2 class="orange-text text-center"><mdb-icon icon="sun" size="2x" /><strong> SPRING SCHEDULE </strong><mdb-icon icon="sun" size="2x" /></h2>
 	<mdb-row class="mt-5 align-items-center justify-content-left p-3" id="winter">
 
-    <h4 class="blue-text">Winter schedule starts January 2!</h4> 
-				<a href="https://dancestudio-pro.com/online/index.php?account_id=15325" target="_blank"><mdb-btn class="btn-grad2">REGISTER FOR WINTER SESSIONS!</mdb-btn></a>
+    <!-- <h4 class="blue-text">Spring Classes!</h4>  -->
+				<a href="https://dancestudio-pro.com/online/index.php?account_id=15325" target="_blank"><mdb-btn class="btn-grad2">REGISTER FOR SPRING SESSION!</mdb-btn></a>
 	
     </mdb-row>
 		<h4>Monday</h4>
@@ -97,9 +96,9 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 					</tr>
 				</mdb-tbl-head>
 				<mdb-tbl-body>
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row" class="deep-orange lighten-3">
 						<td>12:00pm-1:00pm</td>
-						<td>Beginner 1-2 (1-2 year experience)<br/>Denise Caston-Clark</td>
+						<td>Beginner 1-2 (1-2 years experience)<br/>Denise Caston-Clark</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
@@ -109,7 +108,7 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row" class="deep-orange lighten-3">
 						<td>5:00pm-5:30pm</td>
 						<td>First Shuffles (4-5 year olds)</td>
 						<td>RENTAL AVAILABLE</td>
@@ -121,7 +120,7 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-                    <tr scope="row" class="blue lighten-4">
+                    <tr scope="row" class="deep-orange lighten-3">
 						<td>7:30pm-9:00pm</td>
 						<td>Beginner 1-2 (1-3 year experience) <br/> Denise Caston-Clark</td>
 						<td>RENTAL AVAILABLE</td>
@@ -143,7 +142,7 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 				</mdb-tbl-head>
 				<mdb-tbl-body>
 				
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row" class="deep-orange lighten-3">
 						<td>11:30am-12:00pm</td>
 						<td><strong>11:30am-12:00pm Wee Shuffles (2-3 year olds)</strong> <br/> Denise Caston-Clark</td>
 						<td>RENTAL AVAILABLE</td>
@@ -151,11 +150,11 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 					</tr>
           <tr scope="row">
 						<td>12:00pm-1:00pm</td>
-						<td>Beginner 1-2 (1-2 year experience) <br/> Denise Caston-Clark</td>
+						<td>Beginner 1-2 (1-2 years experience) <br/> Denise Caston-Clark</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-                    <tr scope="row" class="blue lighten-4">
+                    <tr scope="row" class="deep-orange lighten-3">
 						<td>4:00pm-5:00pm</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
@@ -167,9 +166,9 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-					<tr scope="row" class="blue lighten-4">
-						<td>6:30pm-7:30pm</td>
-						<td>RENTAL AVAILABLE</td>
+					<tr scope="row" class="deep-orange lighten-3">
+						<td>6:00pm-7:00pm</td>
+						<td>XTreme Hip Hop<br/> by Tonya and Nicole<br/>Read about Xtreme Hip Hop <a class="red-text" href="https://www.xtremefitnesswithphil.com/about" target="_blank">here</a>.<br/>$10 per class</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
@@ -194,7 +193,7 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 					</tr>
 				</mdb-tbl-head>
 				<mdb-tbl-body>
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row" class="deep-orange lighten-3">
 						<td>1:00pm-2:00pm</td>
 						<td>Beginner 1 (0-1 year experience) <br/>Nia Silao </td>
 						<td>RENTAL AVAILABLE</td>
@@ -206,27 +205,27 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row" class="deep-orange lighten-3">
 						<td>5:00pm-5:30pm</td>
 						<td>Jazz 1 Winter 1 session, 4-6 year-olds</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 						
 					</tr>
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row">
 						<td>5:30pm-6:30pm</td>
 						<td>Tap Fundamentals for Teens</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-					<tr scope="row">
+					<tr scope="row"  class="deep-orange lighten-3">
 						<td>6:30pm-7:30pm</td>
 						<td>ABSOLUTE Beginner Series for Adults</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
 					
-          <tr scope="row" class="blue lighten-4">
+          <tr scope="row">
 						<td>7:30pm-9:00pm</td>
 						<td>Beginner 1-2 (1-3 year experience) <br/> Denise Caston-Clark</td>
 						<td>RENTAL AVAILABLE</td>
@@ -248,7 +247,7 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 				</mdb-tbl-head>
 				<mdb-tbl-body>
 				
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row" class="deep-orange lighten-3">
 						<td>11:30am-12:00pm</td>
 						<td><strong>11:30am-12:00pm Wee Shuffles (2-3 year olds)</strong> <br/> Denise Caston-Clark</td>
 						<td>RENTAL AVAILABLE</td>
@@ -256,11 +255,11 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 					</tr>
 					<tr scope="row">
 						<td>12:00pm-1:00pm</td>
-						<td>Beginner 1-2 (1-2 year experience)<br/>Denise Caston-Clark</td>
+						<td>Beginner 1-2 (1-2 years experience)<br/>Denise Caston-Clark</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row" class="deep-orange lighten-3">
 						<td>12:30pm-4:30pm</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
@@ -272,10 +271,10 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-                    <tr scope="row" class="blue lighten-4">
+                    <tr scope="row" class="deep-orange lighten-3">
 						<td>6:00pm-7:30pm</td>
 						<td><strong>6:00pm-6:45pm Young Shuffles Tap 1(8-10 year olds)</strong><br/>Denise Caston-Clark<hr/><strong>6:45pm-7:30pm</strong><br/>Jazz 1 (8-10 year olds)</td>
-						<td><strong>6:30pm-7:30pm</strong><mdb-btn @click.native="showJazz = true" outline="blue">Beginner 1 Jazz</mdb-btn>
+						<td><strong>6:30pm-7:30pm</strong><mdb-btn @click.native="showJazz = true" outline="white">Beginner 1 Jazz</mdb-btn>
         <mdb-modal :show="showJazz" @close="showJazz = false" cascade class="text-left">
           <mdb-modal-header class="red white-text">
             <h4 class="title">Beginner 1 Jazz</h4>
@@ -286,7 +285,7 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
           </mdb-modal-body>
          
         </mdb-modal><br/>Lisa Allai-Stop<br/></td>
-						<td>RENTAL AVAILABLE</td>
+						<td><strong>6:00pm-7:00pm</strong>XTreme Hip Hop<br/> by Tonya and Nicole<br/>Read about Xtreme Hip Hop <a class="red-text" href="https://www.xtremefitnesswithphil.com/about" target="_blank">here</a>.<br/>$10 per class</td>
 					</tr>
                     <tr scope="row">
 						<td>7:30pm-9:00pm</td>
@@ -309,7 +308,7 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
 					</tr>
 				</mdb-tbl-head>
 				<mdb-tbl-body>
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row" class="deep-orange lighten-3">
 						<td>12:00pm-1:00pm</td>
 						<td>Beginner 1 (0-1 year experience)<br/>Denise Caston-Clark</td>
 						<td>RENTAL AVAILABLE</td>
@@ -321,7 +320,7 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row" class="deep-orange lighten-3">
 						<td>5:00pm-6:00pm</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
@@ -330,7 +329,7 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
 					<tr scope="row">
 						<td>6:00pm-7:30pm</td>
 						<td>RENTAL AVAILABLE</td>
-						<td><mdb-btn @click.native="showRock = true" outline="blue">ROCKette ON Jazz</mdb-btn>
+						<td><mdb-btn @click.native="showRock = true" outline="orange">ROCKette ON Jazz</mdb-btn>
         <mdb-modal :show="showRock" @close="showRock = false" cascade class="text-left">
           <mdb-modal-header class="red white-text">
             <h4 class="title">ROCKette ON Jazz</h4>
@@ -343,7 +342,7 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
 		Denise Caston-Clark</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-                    <tr scope="row" class="blue lighten-4">
+                    <tr scope="row" class="deep-orange lighten-3">
 						<td>7:30pm-9:00pm</td>
 						<td>Intermediate 2 (5+ years) <br/>Jer Vernier</td>
 						<td>Beginner 1-2 (1-3 year experience)<br/>Denise Caston-Clark</td>
@@ -364,16 +363,22 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
 					</tr>
 				</mdb-tbl-head>
 				<mdb-tbl-body>
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row" class="deep-orange lighten-3">
+						<td>8:00am-9:00am</td>
+						<td>XTreme Hip Hop<br/> by Tonya and Nicole<br/>Read about Xtreme Hip Hop <a class="red-text" href="https://www.xtremefitnesswithphil.com/about" target="_blank">here</a>.<br/>$10 per class</td>
+						<td>RENTAL AVAILABLE</td>
+						<td>RENTAL AVAILABLE</td>
+					</tr>
+					<tr scope="row">
 						<td>11:00am-12:00pm</td>
 						<td><strong>Tap/Jazz combo</strong> (6-8 year-olds)</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-					<tr scope="row">
+					<tr scope="row" class="deep-orange lighten-3">
 						<td>12:00pm-12:45pm</td>
 						<td></td>
-						<td><a href="http://www.detroityogafit.com" target="_blank"><mdb-btn outline="blue">YogaFit Basics</mdb-btn></a><br/>$12</td>
+						<td><a href="http://www.detroityogafit.com" target="_blank"><mdb-btn outline="white">YogaFit Basics</mdb-btn></a><br/>$12</td>
 						<td>RENTAL AVAILABLE</td>
 					<tr scope="row">
 						<td>12:30pm-2:00pm</td>
@@ -381,15 +386,36 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-					<tr scope="row" class="blue lighten-4">
+					<tr scope="row">
 						<td>2:00pm-3:30pm</td>
 						<td>Beginner 1 (0-1 year experience)<br/> Lisa Allai-Stop </td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
-                    <tr scope="row">
+                    <tr scope="row" class="deep-orange lighten-3">
 						<td>3:30pm-5:00pm</td>
 						<td>Intermediate 1 (4+ years experience)<br/> Lisa Allai-Stop </td>
+						<td>RENTAL AVAILABLE</td>
+						<td>RENTAL AVAILABLE</td>
+					</tr>
+				</mdb-tbl-body>
+			</mdb-tbl>
+		</section>
+<h4>SUNDAY</h4>
+		<section>
+			<mdb-tbl bordered>
+				<mdb-tbl-head>
+					<tr>
+						<th>Time</th>
+						<th>Studio 1</th>
+						<th>Studio 2</th>
+						<th>Studio 3</th>
+					</tr>
+				</mdb-tbl-head>
+				<mdb-tbl-body>
+                    <tr scope="row" class="deep-orange lighten-3">
+						<td>5:00pm-6:00pm</td>
+						<td><strong>Every 4th Sunday of the Month!</strong><br/>XTreme Hip Hop<br/> by Tonya and Nicole<br/>Read about Xtreme Hip Hop <a class="red-text" href="https://www.xtremefitnesswithphil.com/about" target="_blank">here</a>.<br/>$10 per class</td>
 						<td>RENTAL AVAILABLE</td>
 						<td>RENTAL AVAILABLE</td>
 					</tr>
@@ -417,7 +443,7 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
     </div>
     <div class="align-items-center justify-content-start pl-3">
     <span class="red-text"><mdb-icon far icon="calendar-alt" size="2x"/><strong class="big"> Tuesdays or Thursdays 11:30am-12:00pm</strong> </span>
-	<p class="big"><strong> January 7-February 25 - $100</strong></p>
+	<p class="big"><strong> 8-week Session: $100</strong></p>
 
 	</div>
     <div class="mt-5 align-items-center justify-content-start p-3">
@@ -426,8 +452,8 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
     <div class="align-items-center justify-content-start pl-3">
     <span class="red-text"><mdb-icon far icon="calendar-alt" size="2x"/> <strong class="big">Mondays or Wednesdays 4:30pm-5:00pm</strong></span>
     <p class="big">This session is for 4-6 year-olds to discover the joy of tap dancing, learning basic steps and participating in fun interactive activities that will teach them spacial awareness and musicality. The dancers will show off what they've learned after the last class.</p>
-	<p class="big"><strong>Winter 1: 8-week January 3-February 24: $100</strong></p>
-	<p class="big"><strong>Winter 1: Shuffles and Chassés Tap/Jazz Bundle - January 4-February 24: $175. Mondays or Wednesdays 4:30pm-5:30pm</strong></p>
+	<p class="big"><strong> 8-week Session: $100</strong></p>
+	<p class="big"><strong>Shuffles and Chassés Tap/Jazz Bundle: $175. Mondays or Wednesdays 4:30pm-5:30pm</strong></p>
 	</div>
   <div class="mt-5 align-items-center justify-content-start p-3">
     <h4 class="deep-orange-text big">6-8 YEAR-OLDS</h4>
@@ -435,7 +461,7 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
     <div class="align-items-center justify-content-start pl-3">
     
     <span class="red-text"><mdb-icon far icon="calendar-alt" size="2x"/> <strong class="big">Saturdays 11:00am-12:00pm</strong></span>
-    <p class="big"><strong>Tap/Jazz Combo</strong> 8-week session with Denise Caston-Clark from January 4-February 22. $140 </p>
+    <p class="big"><strong>Tap/Jazz Combo</strong> 8-week session with Denise Caston-Clark. $140 </p>
   </div> 
 
   <div class="mt-5 align-items-center justify-content-start p-3">
@@ -457,7 +483,7 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
     <div class="align-items-center justify-content-start pl-3">
     
     <span class="red-text"><mdb-icon far icon="calendar-alt" size="2x"/> <strong class="big">Wednesdays 5:30pm-6:30pm</strong></span>
-    <p class="big"><strong>Tap Fundamentals for Teens</strong>8 weeks, January 8-February 26. $140</p>
+    <p class="big"><strong>Tap Fundamentals for Teens</strong> 8 weeks: $140</p>
 	
   </div> 
 	<!-- <div class="mt-5 align-items-center justify-content-start p-3">
@@ -474,8 +500,8 @@ In addition to being a beautiful art form, jazz classes will stretch you, streng
        <img src="/static/first-second-shuffles.jpg" class="img-fluid-grow mx-auto d-block" style="max-width: 100%"/>
 	    <div class="align-items-center justify-content-center p-5">
       <!-- <mdb-col size="12" class="text-center mb-5"> -->
-        <a href="#schedule"><mdb-btn class="btn-grad"><mdb-icon fab icon="canadian-maple-leaf" size="2x" /><br/>VIEW FULL FALL SCHEDULE</mdb-btn></a>
-		<a href="#winter"><mdb-btn class="btn-grad2"><mdb-icon icon="snowflake" size="2x" /><br/>VIEW FULL WINTER SCHEDULE</mdb-btn></a>
+        <a href="#schedule"><mdb-btn class="btn-grad"><mdb-icon icon="sun" size="2x" /><br/>VIEW FULL SPRING SCHEDULE</mdb-btn></a>
+		<!-- <a href="#winter"><mdb-btn class="btn-grad2"><mdb-icon icon="snowflake" size="2x" /><br/>VIEW FULL WINTER SCHEDULE</mdb-btn></a> -->
         
     </div>
    </mdb-col>
@@ -563,7 +589,7 @@ body {
 .btn-grad {background-image: linear-gradient(to right, #FF512F 0%, #F09819 51%, #FF512F 100%)}
 .btn-grad:hover { background-position: right center; }
 
-.btn-grad2 {background-image: linear-gradient(to right, #3F69FF 0%, #80DFFF 51%, #3F69FF 100%)}
+.btn-grad2 {background-image: linear-gradient(to right, #FF512F 0%, #F09819 51%, #FF512F 100%)}
 .btn-grad:hover { background-position: right center; }
 
 </style>
