9425c2f393d84aefe62e0536b0093c2e06f5568a
Jaffa Aharonov <j.aharonov@gmail.com>
2021-02-18 22:58:52 -0600

    mindbody

--- src/components/Schedule.vue
+++ src/components/Schedule.vue
@@ -73,7 +73,7 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 		<mdb-row class="text-justify">
     <div class="align-items-center justify-content-start p-3">
       <!-- <mdb-col size="12" class="text-center mb-5"> -->
-				<a href="https://dancestudio-pro.com/online/index.php?account_id=15325" target="_blank"><mdb-btn outline="red">Register for classes!</mdb-btn></a>
+				<a href="https://www.mindbodyonline.com/explore/locations/tap-dance-detroit" target="_blank"><mdb-btn outline="red">Register for classes!</mdb-btn></a>
 	</div>
 	</mdb-row>
 <section class="demo-section pt-5" id="schedule">
@@ -81,334 +81,16 @@ We will make exceptions! Just notify the studio ahead of time if a dancer under 
 	<mdb-row class="mt-5 align-items-center justify-content-left p-3" id="winter">
 
     <!-- <h4 class="blue-text">Spring Classes!</h4>  -->
-				<a href="https://dancestudio-pro.com/online/index.php?account_id=15325" target="_blank"><mdb-btn class="btn-grad2">REGISTER FOR WINTER SESSION!</mdb-btn></a>
-	<iframe src="https://dancestudio-pro.com/apps/api_classes_resp.php?id=zaqlxajd29jd25cfea9701936309jasdklj21dx5cfea970193a1" width="100%" height="600px" scrolling="yes"
-frameborder="0"></iframe>
+				<a href="https://www.mindbodyonline.com/explore/locations/tap-dance-detroit" target="_blank"><mdb-btn class="btn-grad2">REGISTER FOR WINTER SESSION!</mdb-btn></a>
+	<!-- <iframe src="https://dancestudio-pro.com/apps/api_classes_resp.php?id=zaqlxajd29jd25cfea9701936309jasdklj21dx5cfea970193a1" width="100%" height="600px" scrolling="yes"
+frameborder="0"></iframe> -->
+<healcode-widget data-type="schedules" data-widget-partner="object" data-widget-id="381544197c15" data-widget-version="1" ></healcode-widget>
     </mdb-row>
-		<!-- <h4>Monday</h4>
-		<section>
-			<mdb-tbl bordered>
-				<mdb-tbl-head>
-					<tr>
-						<th>Time</th>
-						<th>Studio 1</th>
-						<th>Studio 2</th>
-						<th>Studio 3</th>
-					</tr>
-				</mdb-tbl-head>
-				<mdb-tbl-body>
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>12:00pm-1:00pm</td>
-						<td>Beginner 1-2 (1-2 years experience)<br/>Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row">
-						<td>4:30pm-5:00pm</td>
-						<td><strong>Wee Shuffles (2-3 year olds)</strong> <br/>Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>5:00pm-5:30pm</td>
-						<td>First Shuffles (4-5 year olds)</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row">
-						<td>6:30pm-7:30pm</td>
-						<td>Beginner 1 (0-1 year experience) <br/> Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-                    <tr scope="row" class="deep-orange lighten-3">
-						<td>7:30pm-9:00pm</td>
-						<td>Beginner 1-2 (1-3 year experience) <br/> Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-				</mdb-tbl-body>
-			</mdb-tbl>
-		</section>
-        <h4>Tuesday</h4>
-        <section>
-			<mdb-tbl bordered>
-				<mdb-tbl-head>
-					<tr>
-						<th>Time</th>
-						<th>Studio 1</th>
-						<th>Studio 2</th>
-						<th>Studio 3</th>
-					</tr>
-				</mdb-tbl-head>
-				<mdb-tbl-body>
-				
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>11:30am-12:00pm</td>
-						<td><strong>11:30am-12:00pm Wee Shuffles (2-3 year olds)</strong> <br/> Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-          <tr scope="row">
-						<td>12:00pm-1:00pm</td>
-						<td>Beginner 1-2 (1-2 years experience) <br/> Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-                    <tr scope="row" class="deep-orange lighten-3">
-						<td>4:00pm-5:00pm</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row">
-						<td>5:30pm-6:30pm</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>6:00pm-7:00pm</td>
-					
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-                    <tr scope="row">
-						<td>7:30pm-9:00pm</td>
-						<td>Intermediate 1-2 Broadway Style <br/>Denise Caston-Clark </td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-				</mdb-tbl-body>
-			</mdb-tbl>
-		</section>
-        <h4>Wednesday</h4>
-		<section>
-			<mdb-tbl bordered>
-				<mdb-tbl-head>
-					<tr>
-						<th>Time</th>
-						<th>Studio 1</th>
-						<th>Studio 2</th>
-						<th>Studio 3</th>
-					</tr>
-				</mdb-tbl-head>
-				<mdb-tbl-body>
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>1:00pm-2:00pm</td>
-						<td>Beginner 1 (0-1 year experience) <br/>Nia Silao </td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row">
-						<td>4:30pm-5:00pm</td>
-						<td><strong> 4:30pm-5:00pm First Shuffles Tap 1 (4-6 year olds) </strong><br/><br/>Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>5:00pm-5:30pm</td>
-						<td>Jazz 1 Winter 1 session, 4-6 year-olds</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-						
-					</tr>
-					<tr scope="row">
-						<td>5:30pm-6:30pm</td>
-						<td>Tap Fundamentals for Teens</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row"  class="deep-orange lighten-3">
-						<td>6:30pm-7:30pm</td>
-						<td>ABSOLUTE Beginner Series for Adults</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					
-          <tr scope="row">
-						<td>7:30pm-9:00pm</td>
-						<td>Beginner 1-2 (1-3 year experience) <br/> Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-				</mdb-tbl-body>
-			</mdb-tbl>
-		</section>
-        <h4>Thursday</h4>
-        <section>
-			<mdb-tbl bordered>
-				<mdb-tbl-head>
-					<tr>
-						<th>Time</th>
-						<th>Studio 1</th>
-						<th>Studio 2</th>
-						<th>Studio 3</th>
-					</tr>
-				</mdb-tbl-head>
-				<mdb-tbl-body>
-				
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>11:30am-12:00pm</td>
-						<td><strong>11:30am-12:00pm Wee Shuffles (2-3 year olds)</strong> <br/> Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row">
-						<td>12:00pm-1:00pm</td>
-						<td>Beginner 1-2 (1-2 years experience)<br/>Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>12:30pm-4:30pm</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-                    <tr scope="row">
-						<td>5:00pm-6:00pm</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-                    <tr scope="row" class="deep-orange lighten-3">
-						<td>6:00pm-7:30pm</td>
-						<td><strong>6:00pm-6:45pm Young Shuffles Tap 1(8-10 year olds)</strong><br/>Denise Caston-Clark<hr/><strong>6:45pm-7:30pm</strong><br/>Jazz 1 (8-10 year olds)</td>
-						<td><strong>6:30pm-7:30pm</strong><mdb-btn @click.native="showJazz = true" outline="white">Beginner 1 Jazz</mdb-btn>
-        <mdb-modal :show="showJazz" @close="showJazz = false" cascade class="text-left">
-          <mdb-modal-header class="red white-text">
-            <h4 class="title">Beginner 1 Jazz</h4>
-          </mdb-modal-header>
-          <mdb-modal-body class="grey-text">
-            <p>If you've always wanted to try jazz or you did it a long time ago in a galaxy far, far away, this is a great class to get started!
-In addition to being a beautiful art form, jazz classes will stretch you, strengthen you, and you get to do it with some attitude.</p>
-          </mdb-modal-body>
-         
-        </mdb-modal><br/>Lisa Allai-Stop<br/></td>
-		<td>RENTAL AVAILABLE</td>
-					
-					</tr>
-                    <tr scope="row">
-						<td>7:30pm-9:00pm</td>
-						<td>Intermediate 1-2 Broadway Style Tap<br/>Denise Caston-Clark </td>
-						<td>Intermediate 1 (4+ years experience) <br/>Lisa Allai-Stop</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-				</mdb-tbl-body>
-			</mdb-tbl>
-		</section>
-        <h4>FRIDAY</h4>
-		<section>
-			<mdb-tbl bordered>
-				<mdb-tbl-head>
-					<tr>
-						<th>Time</th>
-						<th>Studio 1</th>
-						<th>Studio 2</th>
-						<th>Studio 3</th>
-					</tr>
-				</mdb-tbl-head>
-				<mdb-tbl-body>
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>12:00pm-1:00pm</td>
-						<td>Beginner 1 (0-1 year experience)<br/>Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row">
-						<td>3:00pm-4:30pm</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>5:00pm-6:00pm</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row">
-						<td>6:00pm-7:30pm</td>
-						<td>RENTAL AVAILABLE</td>
-						<td><mdb-btn @click.native="showRock = true" outline="orange">ROCKette ON Jazz</mdb-btn>
-        <mdb-modal :show="showRock" @close="showRock = false" cascade class="text-left">
-          <mdb-modal-header class="red white-text">
-            <h4 class="title">ROCKette ON Jazz</h4>
-          </mdb-modal-header>
-          <mdb-modal-body class="grey-text">
-            <p>Learn the precision style jazz of the Radio City Rockettes from a former Rockette herself!</p>
-          </mdb-modal-body>
-         
-        </mdb-modal><br/>Advanced Beginner <br/>
-		Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-                    <tr scope="row" class="deep-orange lighten-3">
-						<td>7:30pm-9:00pm</td>
-						<td>Intermediate 2 (5+ years) <br/>Jer Vernier</td>
-						<td>Beginner 1-2 (1-3 year experience)<br/>Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-				</mdb-tbl-body>
-			</mdb-tbl>
-		</section>
-        <h4>SATURDAY</h4>
-		<section>
-			<mdb-tbl bordered>
-				<mdb-tbl-head>
-					<tr>
-						<th>Time</th>
-						<th>Studio 1</th>
-						<th>Studio 2</th>
-						<th>Studio 3</th>
-					</tr>
-				</mdb-tbl-head>
-				<mdb-tbl-body>
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>8:00am-9:00am</td>
-
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row">
-						<td>11:00am-12:00pm</td>
-						<td><strong>Tap/Jazz combo</strong> (6-8 year-olds)</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row" class="deep-orange lighten-3">
-						<td>12:00pm-12:45pm</td>
-						<td></td>
-						<td><a href="http://www.detroityogafit.com" target="_blank"><mdb-btn outline="white">YogaFit Basics</mdb-btn></a><br/>$12</td>
-						<td>RENTAL AVAILABLE</td>
-					<tr scope="row">
-						<td>12:30pm-2:00pm</td>
-						<td>Intermediate 1-2 (4+ years experience)<br/>Denise Caston-Clark</td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-					<tr scope="row">
-						<td>2:00pm-3:30pm</td>
-						<td>Beginner 1 (0-1 year experience)<br/> Lisa Allai-Stop </td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-                    <tr scope="row" class="deep-orange lighten-3">
-						<td>3:30pm-5:00pm</td>
-						<td>Intermediate 1 (4+ years experience)<br/> Lisa Allai-Stop </td>
-						<td>RENTAL AVAILABLE</td>
-						<td>RENTAL AVAILABLE</td>
-					</tr>
-				</mdb-tbl-body>
-			</mdb-tbl>
-		</section> -->
+		
 		
 		<mdb-row class="text-justify">
     <div class="align-items-center justify-content-start p-3">
-				<a href="https://dancestudio-pro.com/online/index.php?account_id=15325" target="_blank"><mdb-btn class="btn-grad2">Register for classes!</mdb-btn></a>
+				<a href="https://www.mindbodyonline.com/explore/locations/tap-dance-detroit" target="_blank"><mdb-btn class="btn-grad2">Register for classes!</mdb-btn></a>
 	</div>
 	</mdb-row>
 	</section>
@@ -514,6 +196,11 @@ import { mdbTbl, mdbTblHead, mdbTblBody, mdbContainer, mdbRow, mdbCol, mdbNavbar
       mdbModalBody,
       mdbModalFooter,
     },
+	mounted() {
+    let externalScript = document.createElement('script')
+    externalScript.setAttribute('src', 'https://widgets.mindbodyonline.com/javascripts/healcode.js')
+    document.head.appendChild(externalScript)
+  },
     data() {
       return {
 		showModal: false,
