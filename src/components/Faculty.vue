b3e670dd5b1d9225cbd8110bd3f91fc19f9e4cc4
Jaffa Aharonov <j.aharonov@gmail.com>
2021-02-26 18:16:09 -0600

    faculty updates

--- src/components/Faculty.vue
+++ src/components/Faculty.vue
@@ -21,120 +21,44 @@
 </mdb-col>
     </mdb-row>
     <mdb-row class="mx-auto pt-5">
-      <!-- <mdb-col lg="4" class="pt-4">
-          <img src="/static/faculty_kelsey-rose-young.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
-      </mdb-col> -->
-      <!-- <mdb-col lg="8" class="text-justify pt-3">
-        <h3 class="red-text mx-auto text-left big">KELSEY ROSE YOUNG</h3>
-          <p>Kelsey Rose Young is a performer, teacher, and choreographer based in Detroit, Michigan. Kelsey is on faculty with Showstopper Dance Convention, Fuzion Dance Camp, and Motor City Tap Fest. She has recently taught at the Big Apple Tap Festival and the Phoenix Tap Festival.</p>
 
-<p>In 2017 she started the Wing It Tap Intensive which travels throughout the country with numerous top instructors. She is the current director and lead choreographer for the pre-professional tap company – Detroit Tap Repertory, as well as a member of Shelby Kaufman’s tap company, G.L.Y.D.E. and Suzy Guarino- Hall’s company, TAP 24.7.</p>
-
-<p>Kelsey has performed with the Detroit Symphony Orchestra as part of the DSO Educational Series and was a guest speaker at the Moving With Detroit seminar for dance educators. In 2015, she was a contestant on the FOX Network's So You Think You Can Dance - Season 12, and season 14. In 2018, Kelsey traveled to Germany to compete in the IDO World Tap Championships. It’s there that her 3 pieces placed 4th and 5th places in the World. She also came in 2nd for the World’s Battle of the Feet.</p>
-
-<p>She has had the privilege of studying under and performing alongside many of today’s most respected tap dancers and legends including Arthur Duncan, Ardie Bryant, Denise Caston, Gregg Russell, Chloe Arnold, Sarah Reich, Chris Broughton, Jenefer Miller, Suzy Guarino-Hall, and Dianne Walker.</p>
-
-<p>As a choreographer, Kelsey draws from her training and range of experiences as a singer and dancer to create a style that is intricate, gritty, passionate, and entertaining. Her pieces have received many awards and accolades across the country and she is quickly becoming one of the Midwest’s premier tap choreographers.</p>
-
-<p>Kelsey is a member of Cecchetti Council of America and has also studied and competed in ballroom dancing. She works independently as a choreographer for students throughout the Nation. Over the years, Kelsey has worked with Jay Sean, the Jonas Brothers, Debbie Gibson, and Radio Disney. She has performed in the Detroit, Macy’s and Chicago Thanksgiving Day Parades, at the Detroit Jazz Festival, and at TedX Detroit. She most recently has performed and choreographed a half time show for the NBA’s Detroit Pistons.</p>
-</mdb-col> -->
+    </mdb-row>
+    <mdb-row class="mx-auto pt-2">
+        <h1 class="mx-auto text-center display-4 p-3 red-text big">LIVESTREAM FACULTY</h1>
     </mdb-row>
     <mdb-row class="mx-auto pt-5">
       <mdb-col lg="4" class="pt-4">
-          <img src="/static/faculty_nia-silao.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
+          <img src="/static/RebeccaFrazierBioPhoto.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
       </mdb-col>
       <mdb-col lg="8" class="text-justify pt-3">
         <h3 class="red-text mx-auto text-left big">NIA SILAO</h3>
-          <p>Nia Silao has been a professional dance educator for 10 years and her passion is enriching dance in Michigan. Her credits include Bachelors Degree in Dance Wayne State University; Part-time Faculty Wayne State University; Certified with Dance Masters of America and Dance Masters of Michigan; Official tutor for Dance Masters of Michigan exams; Faculty Brighton Dance Festival and Lenawee Arts Festival; Selected VSA Michigan Teaching Artist; Adjudicator for various competitions and scholarship auditions; Dance Department at Music Hall Detroit; Assistant to Director Motor City Tap Festival, spearhead of the Modern Dance Program at American Dance Academy; leader of Adult Dance Program at Dance Academy of Bloomfield Hills; guest teacher at Cranbrook, Rochester School of Dance, Spotlight Dance Center, The Dance Shoppe, Milford Dance Academy. Her works have won Choreography awards, Technique awards, and Creative Concept awards. Nia also performed and toured with DDCdances, Eisenhower Dance Ensemble, LaMarre and Dancers, Cloud 9 Special Events, and Luna Dancers. Nia has performed to raise donations for causes such as Alternatives for Girls and Brighton Center for the Performing Arts. She has volunteered with Dance Masters VIP Junior Members encouraging young dancers to find their own path to dance education. Her students have won various scholarships, gone to work on Broadway, with touring companies, and for Disney Corporation, in addition to competing in the World Tap Dance Championships.</p>
+          <p>Rebecca Frazier trained at the Albany Dance Institute under David Otto, a former soloist at New York City Ballet, and one of the last dancers to be selected for the company by Balanchine. Rebecca was selected by children’s ballet mistress Garielle Whittle to perform children’s roles for NYCB during their summer residence at the Saratoga Performing Arts Center. Rebecca received additional training at the Saratoga Summer Dance Intensive and at the Ballet Regent School in Saratoga Springs, from former NYCB Michael Steele. Rebecca danced with Albany Berkshire Ballet. She is a member of Actor’s Equity Association and has performed in regional theaters around the country and in New York City.
+
+Rebecca is the choreographer for recording artist JR Price.  She was the choreographer for the award-winning short film Dream No 37, 091 .In New York City, Rebecca has presented work at Dixon Place, NYC Choreographers Forum, National Choreography Month (NACHMO), American Liberty Ballet, Fertile Ground, and NachmoX. Rebecca was a featured choreographer at the New York Theater Barn’s Choreography Lab. Her dance film installation “Selbstliebe” is part of Robot Love Art at the Foosaner Art Museum in Melborne, FL.
+
+She currently is on the faculty of The Dance Pointe in Greenwich, CT, and Just Off Broadway Dance Academy in New Jersey. She has guest taught for Just off Broadway, Eastside Dance Company, and the Salt City Dance Festival. </p>
 </mdb-col>
-    </mdb-row>
-    <mdb-row class="mx-auto pt-5">
-      <!-- <mdb-col lg="4" class="pt-4">
-          <img src="/static/faculty_jamie-hetherington.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
-      </mdb-col>
-      <mdb-col lg="8" class="text-justify pt-3">
-        <h3 class="red-text mx-auto text-left big">JAMIE HETHERINGTON</h3>
-          <p>Jamie has been tap dancing for  over 25 years. The majority of her dance training was at The Dance Shoppe, where she is now part of the faculty. She loves sharing her passion for tap.
-Jamie has a BFA from GVSU, with an emphasis in ceramics. She is happily married with 2 awesome boys.</p>
-</mdb-col> -->
-    </mdb-row>
-    <mdb-row class="mx-auto pt-5">
-      <!-- <mdb-col lg="4" class="pt-4">
-          <img src="/static/faculty_paige-baidel.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
-      </mdb-col>
-      <mdb-col lg="8" class="text-justify pt-3">
-        <h3 class="red-text mx-auto text-left big">PAIGE BAIDEL</h3>
-          <p>Paige Baidel is a performer, teacher, and business professional currently residing in Milford, Michigan. She trained in various dance styles from the age of 3, but has always been most passionate about tap dancing. Paige currently serves as Coordinator for the pre-professional tap company- the Detroit Tap Repertory and has performed across Metro Detroit, including America’s Thanksgiving Day Parade, Detroit Pistons Halftime, Channel 7 WXYZ – TV, Fox 2 Detroit News, Orchestra Hall and Music Hall. 
-            She has also traveled to Phoenix, Arizona to perform in various Tap 24.7 shows under the direction of Suzy Guarino-Hall, performed at the Big Apple Tap Festival and was an Assistant on Faculty to Ray Hesselink, Shelby Kaufman and Vikas Arun at Motor City Tap Fest, along with being a member of the Pre-Pro Crew. At Tap City - The New York City Tap Festival, Paige has been a work study and trained with Sam Weber and Brenda Bufalino in the adult residency program. Additionally, 
-            she has had the pleasure of studying under various acclaimed tap dancers and choreographers, such as Karen Callaway-Williams, Denise Caston, Barbara Duffy, Savion Glover, Jenefer Miller, Sarah Reich, Gregg Russell, Dianne Walker and numerous others. Paige has always loved tap dancing and continues to be inspired by legendary tap masters, history, music, travel, life experiences and her dance students.</p>
-</mdb-col> -->
     </mdb-row>
     <mdb-row class="mx-auto pt-5">
       <mdb-col lg="4" class="pt-4">
           
-          <img src="/static/faculty_jer-vernier.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
+          <img src="/static/faculty_jenefer-miller.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
       </mdb-col>
       <mdb-col lg="8" class="text-justify pt-3">
-        <h3 class="red-text mx-auto text-left big">JER VERNIER</h3>
-          <p>Jer Vernier spent much of his early years training at Center for Creative Studies in Detroit, where I was a member of the Tap Repertory Ensemble. After graduating high school he majored in Dance at Marygrove College and also studied at Edge Performing Arts Center in Hollywood, CA.</p>
-          <p>He has performed with Dance Detroit, Detroit Tap Repertory, Detroit Symphony Orchestra and in multiple venues across the United States and Canada.</p>
-          <p>Jer knew from his first dance class that teaching others was going to be his calling in life, and at age 14 began conducting dance classes on his own. He has taught countless students throughout Metro Detroit and have won numerous awards for his choreography.</p>
+        <h3 class="red-text mx-auto text-left big">JENEFER ARDELL MILLER</h3>
+          <p>Jenefer is a native of Michigan and is a tap dancer, teacher, choreographer, director, writer, and producer.  She holds a Bachelor of Arts in the Performing Arts from Oakland University and a Master of Arts in Education from the University of Phoenix.  Jenefer recently performed with the MusicaNova Orchestra and was commissioned to interpret and perform Morton Gould’s Tap Dance Concerto at the Musical Instrument Museum  in Phoenix.  Ms. Miller was also featured in a short film by Screenvision Media’s Corner Stories in 2019 which aired in movie theatres across the United States. She has appeared on stage with Tap 24.7, Mike Wittmer’s Scuff'd Up, Especially Tap Chicago, the Detroit Tap Festival honoring the legendary Nicholas Brothers, Dance Chicago, Sesame Street Live,  and Oakland Dance Theatre. Mentored by Dianne Walker and the late Robert Reed,  Jen has performed in shows with legends such as the Nicholas Brothers, Ben Vereen, Debbie Allen, Savion Glover, Chloe Arnold, Sarah Reich,  Jason Samuels Smith, Dianne Walker, Jimmy Slyde, Barbara Duffy, Robert Reed, Prince Spencer, Harold "Stumpy" Cromer, and Heather Cornell.</p>
+          
 </mdb-col>
     </mdb-row>
     <mdb-row class="mx-auto pt-5">
       <mdb-col lg="4" class="pt-4">
-          <img src="/static/faculty_lisa-allai-stop.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
+          <img src="/static/faculty_stacey-abeles.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
       </mdb-col>
       <mdb-col lg="8" class="text-justify pt-3">
-        <h3 class="red-text mx-auto text-left big">LISA ALLAI-STOP</h3>
-          <p>Lisa Allai-Stop is devoted to dance education and has been educating children and adults for over 30 years.  She is trained in various dance forms with a strong passion for tap, contemporary and hula (Polynesian dance).  Lisa has had the opportunity to perform professionally alongside the unforgettable Ms. Eartha Kitt at the Detroit Music Hall, the Detroit Piston's Halftime show, live news slots promoting the Motor City Tap Festival and the International Festival and countless other local and national venues and events.</p>
-          <p>Currently, Lisa continues as a performing member of the Detroit Tap Repertory, rehearsal director for the Detroit Tap Repertory Unlimited group and serves as historian for the company.  She is also the director of the Tropical Wahines professional Polynesian Dance troupe.</p>
-          <p>Ms. Allai-Stop has choreographed for the Henry Ford College Full Circle Dance Company, for the Detroit Tap Repertory, Club Di Movimiento in Aruba, Wayne County Community College District and choreographed for local community theatre as well.  Additionally, Lisa has taught for the Detroit Dance City Festival, 
-            The Beast Dance Conventionthe American College Dance Association and has been a guest teacher or competition team judge at numerous studios locally and worldwide including Aruba.  She is the director and owner of Lincoln Park Dance Company and also on the faculty of Eisenhower Dance Center and Stars Elite 
-            Training Academy.  Lisa is the Michigan representative for the International Tap Association.</p>
+        <h3 class="red-text mx-auto text-left big">STACEY ABELES</h3>
+          <p>Stacey Abeles, a native New Yorker, graduated from NYU Tisch with a BFA in Drama.  She also trained at Atlantic Theater Company and Upright Citizens Brigade in NYC, and Trinity College of Music in London.   Favorite stage credits: 9 seasons as a Radio City Rockette, the National Tour of Crazy For You, regional productions of The Who’s Tommy, West Side Story, … Millie, and …Forum.  Film and Television: Julie Taymor’s Across the Universe, The Late Show with David Letterman, Wiener & Wiener, MTV Detox, and national commercials.  Stacey is a proud faculty member of the Stella Adler Studio. She teaches movement and ballet in their conservatory and NYU programs.</p>
+          
 </mdb-col>
-    </mdb-row>
-    <mdb-row class="mx-auto pt-5">
-      <mdb-col lg="4" class="pt-4">
-          <img src="/static/faculty_molly-sute.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
-      </mdb-col>
-      <mdb-col lg="8" class="text-justify pt-3">
-        <h3 class="red-text mx-auto text-left big">MOLLY SUTE</h3>
-          <p>Molly Sute is a Detroit-based dance performer, educator, and choreographer. She is currently an apprentice member of M.A.D.D. Rhythms (Chicago, IL), the director of the tap program at The Dance Academy (Westland, MI).  Molly also recently obtained a dual degree in Dance and Public Relations from Wayne State University.</p>
-          <p>Growing up in a competitive dance program, she developed a diverse foundation and a passion for performing. However, tap quickly became her niche after joining the Detroit Tap Repertory at the age of 12. During her seven years as a dancer and choreographer for DTR, she was able to train under the likes of Professor Robert L. Reed, Sarah Reich, Starinah Dixon, along with developing a network among the Michigan tap dance community. Molly continues to immerse herself in the international tap dance community through M.A.D.D. Rhythms and personal ventures, establishing valuable connections with the industry’s best.</p>
-          <p>On top of recognition for the choreography she sets on her competitive students at The Dance Academy, Molly has worked to establish herself as an emerging choreographer in Michigan’s dance industry. In 2017, she won Motor City Tap Fest’s Young Choreographer’s Competition, and in the same year, was awarded the prestigious Copperfoot Award For Contribution to Repertoire by Wayne State University’s Department of Theatre and Dance. Molly came on as the movement coach and choreographer for The Hilberry Theatre’s production of As You Like It, and she was also nominated for Best Choreography Alongside a Float for the movement she set on the Warrior Strong float in America’s Thanksgiving Day Parade in 2018. </p>
-</mdb-col>
-    </mdb-row>
-    <mdb-row class="mx-auto pt-5">
-      <!-- <mdb-col lg="4" class="pt-4">
-          <img src="/static/faculty_mallory-marshall.png" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
-      </mdb-col>
-      <mdb-col lg="8" class="text-justify pt-3">
-        <h3 class="red-text mx-auto text-left big">MALLORY MARSHALL</h3>
-          <p>At a young 18 years old, Mallory Marshall has established herself as a dance performer, educator and choreographer in the Metro Detroit area. She was a student of The Dance Shoppe in Waterford, MI, where she trained in all subjects of dance for 16 years. In 2016, she became a faculty member at The Dance Shoppe, and began teaching at other studios around MI as well. Mallory has been awarded at numerous competitions and conventions, particularly for her tap dancing, including her most recent title of Dance Masters Miss Dance of America 2019. Mallory has always had a passion for teaching and choreography, and hopes to pursue both at a professional level. She attends Wayne State University 
-            in Detroit as a Dance Major, where she continues to broaden her education and appreciation for the art of dance.</p>
-</mdb-col> -->
-    </mdb-row>
-    <mdb-row class="mx-auto pt-5">
-      <mdb-col lg="4" class="pt-4">
-          <img src="/static/faculty_heidi_schultz.png" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
-      </mdb-col>
-      <mdb-col lg="8" class="text-justify pt-3">
-        <h3 class="red-text mx-auto text-left big">HEIDI SCHULTZ</h3>
-          <p>Heidi Schultz has been teaching tappers of all levels—from absolute beginners to professionals—for more than 30 years. Heidi is the founder, director, and choreographer of two Washington, D.C.-based rhythm tap companies: Tappening and Tappenstance. She loves to tap to a wide range of music including R&B, electronica, reggae, hip hop, Latin, classical, jazz, and rock. She has performed in all types of settings from street festivals to the Kennedy Center in D.C. and Lincoln Center in N.Y.C. After more than 20 years, she is back in Michigan where her tap career started. </p>
-          <p>Heidi has studied, performed, and jammed with finest tap dancers across the U.S. Her tap family includes such greats as Chloe Arnold, Maud Arnold, Bril Barrett (Chicago’s M.A.D.D. Rhythms), Michelle Dorrance, Jason Holley, Jason Janas, Quynn Johnson, Toni Lombre, Joseph Webb, Baakari Wilder, and the Footworks Percussive Dance Ensemble.
-With a MA in Applied Anthropology, she works as a research editor at National Geographic Magazine.
-</p>
-</mdb-col>
-    </mdb-row>
-        <mdb-row class="mx-auto pt-5">
-      <!-- <mdb-col lg="4" class="pt-4">
-          <img src="/static/faculty_emily_rose_merrell.jpg" class="img-fluid-grow mx-auto d-block" style="width: 100%"/>
-      </mdb-col>
-      <mdb-col lg="8" class="text-justify pt-3">
-        <h3 class="red-text mx-auto text-left big">EMILY ROSE MERRELL</h3>
-          <p>Emily Rose has been teaching ballroom dance at franchise and independent studios all over the metro Detroit area since 1996.  Her early training in acting, voice and dance at Flint Youth Theatre, and Theater Arts degree from Kalamazoo College paved the way for her career as a professional actor, dancer, and choreographer.  She has performed with professionals and students alike at showcases, competitions, exhibitions, corporate and charity events.  Emily teaches acting voice, and dance to all ages and enjoys passing along the joy of the performing arts.</p>
-</mdb-col> -->
     </mdb-row>
   </mdb-container>
 </template>
